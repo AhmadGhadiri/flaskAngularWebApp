0914bc9bbeaf4bfc53a8d00a501c3de51dde31a2
Ahmad Ghadiri <ahmad.ghadiri@fireeye.com>
2022-01-12 13:59:29 -0500

    Secured with Auth0

--- backend/src/main.py
+++ backend/src/main.py
@@ -5,6 +5,8 @@ from flask_cors import CORS
 
 from entities.entity import Session, engine, Base
 from entities.exam import Exam, ExamSchema
+from auth import AuthError, requires_auth
+
 
 
 # Creating the Flask application
@@ -29,6 +31,7 @@ def get_exams():
   return jsonify(exams.data)  
 
 @app.route('/exams', methods = ['POST'])
+@requires_auth
 def add_exam():
   # Mount exam object
   posted_exam = ExamSchema(only=('title', 'description')).load(request.get_json())
@@ -44,6 +47,12 @@ def add_exam():
   new_exam = ExamSchema().dump(exam).data
   session.close()
   return jsonify(new_exam), 201
+
+@app.errorhandler(AuthError)
+def handle_auth_error(ex):
+    response = jsonify(ex.error)
+    response.status_code = ex.status_code
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
